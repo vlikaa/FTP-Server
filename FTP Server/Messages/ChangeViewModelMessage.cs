080ea764129381263e4009f6973c3a6153c86755
vlikaa <aliveliyev123654@gmail.com>
2024-09-30 15:51:05 +0400

    added ChangeViewModelMessage

---
+++ FTP Server/Messages/ChangeViewModelMessage.cs
@@ -0,0 +1,8 @@
+﻿using FTP_Server.ViewModels;
+
+namespace FTP_Server.Messages;
+
+public class ChangeViewModelMessage(BaseViewModel viewModel) : IMessage
+{
+	public BaseViewModel ViewModel { get; } = viewModel;
+}
