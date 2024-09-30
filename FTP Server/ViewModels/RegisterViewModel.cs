700f16d74ceedbaaa20d3a07f80e626cee0cc49f
vlikaa <149687088+vlikaa@users.noreply.github.com>
2024-09-30 16:02:07 +0400

    added OpenLoginView command

--- FTP Server/ViewModels/RegisterViewModel.cs
+++ FTP Server/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace FTP_Server.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using FTP_Server.Messages;
+using FTP_Server.Services;
 
-public class RegisterViewModel : BaseViewModel
+namespace FTP_Server.ViewModels;
+
+[INotifyPropertyChanged]
+public partial class RegisterViewModel(ViewModelFactory factory) : BaseViewModel
 {
-	
+	[RelayCommand]
+	private void OpenLoginView()
+	{
+		WeakReferenceMessenger.Default.Send(new ChangeViewModelMessage(factory.Create(typeof(LoginViewModel))));
+	}
 }
