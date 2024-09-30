29ef8fc8e3e549f64d9cc2f882441faf54ad352d
vlikaa <aliveliyev123654@gmail.com>
2024-09-30 14:59:12 +0400

    bux fix & refactoring

--- FTP Server/Controls/CustomTextBox.xaml.cs
+++ FTP Server/Controls/CustomTextBox.xaml.cs
@@ -1,31 +1,33 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
-namespace FTP_Server.Controls;
-
-public partial class CustomTextBox
+namespace FTP_Server.Controls
 {
-	public CustomTextBox()
+	public partial class CustomTextBox : TextBox
 	{
-		InitializeComponent();
+		public CustomTextBox()
+		{
+			InitializeComponent();
 
-		DataContext = this;
-	}
+			DataContext = this;
+		}
 
-	public static readonly DependencyProperty PlaceholderProperty = DependencyProperty.Register(
-		nameof(Placeholder), typeof(string), typeof(CustomTextBox), new PropertyMetadata(default(string)));
+		public static readonly DependencyProperty PlaceholderProperty = DependencyProperty.Register(
+			nameof(Placeholder), typeof(string), typeof(CustomTextBox), new PropertyMetadata(default(string)));
 
-	public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
-		nameof(Title), typeof(string), typeof(CustomTextBox), new PropertyMetadata(default(string)));
-	
-	public string Placeholder
-	{
-		get => (string)GetValue(PlaceholderProperty);
-		set => SetValue(PlaceholderProperty, value);
-	}
-	
-	public string Title
-	{
-		get => (string)GetValue(TitleProperty);
-		set => SetValue(TitleProperty, value);
+		public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+			nameof(Title), typeof(string), typeof(CustomTextBox), new PropertyMetadata(default(string)));
+
+		public string Placeholder
+		{
+			get => (string)GetValue(PlaceholderProperty);
+			set => SetValue(PlaceholderProperty, value);
+		}
+
+		public string Title
+		{
+			get => (string)GetValue(TitleProperty);
+			set => SetValue(TitleProperty, value);
+		}
 	}
 }
